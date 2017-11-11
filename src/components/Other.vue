099820f827df7cc940bc82108f844de61d2fe5a8
jjrajani <jjrajani@gmail.com>
2017-11-11 13:38:18 -0500

    my first vue, data binding

---
+++ src/components/Other.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="other">
+    <h1>{{ show }}</h1>
+    <button @click="toggleVisible()">Toggle Visible</button>
+    <input v-model="text" placeholder="Text">
+    <p>Text is: {{text}}</p>
+    <button @click="submitText()">Submit Text</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Other',
+  data: () => {
+    return { show: false, text: '' }
+  },
+  methods: {
+    toggleVisible: function() {
+      this.show = !this.show
+    },
+    submitText: function() {
+      console.log('text', this.text)
+    }
+  }
+}
+</script>
